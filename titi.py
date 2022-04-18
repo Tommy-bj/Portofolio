7aa3c20e2cccda24068e12f5a79270c46c3a5985
unknown <tommudinike@gmail.com>
2022-04-18 16:10:07 +0200

    make it better

---
+++ titi.py
@@ -0,0 +1,22 @@
+
+
+def main():
+    pass
+    message()
+
+def message():
+    print('Bonjour papa thoms')
+if __name__ == '__main__':
+    print('Bonjour Papa')
+else:
+    print('Bonjour')
+
+def func():
+    pass
+
+
+
+
+if __name__ == '__main__':
+    main()
+
